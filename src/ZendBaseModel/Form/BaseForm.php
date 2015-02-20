bae12d717f9e498585962d62403cb0345f5c70e9
seyfer <seyferseed@mail.ru>
2015-02-20 18:19:53 +0700

    fix base form

--- src/ZendBaseModel/Form/BaseForm.php
+++ src/ZendBaseModel/Form/BaseForm.php
@@ -12,6 +12,19 @@ use Zend\Form\Form;
  */
 abstract class BaseForm extends Form
 {
+    public function __construct($name)
+    {
+        parent::__construct($name);
+
+        $this->addElements();
+    }
 
     abstract protected function addElements();
+
+    protected function addTextElement($name)
+    {
+        $element = new Element\Text($name);
+        $element->setLabel(ucfirst($name));
+        $this->add($element);
+    }
 }
